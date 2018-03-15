84f79c1fd1d7e8260a65df579747ecd81ec62fdc
Juergen Kosel <juergen.kosel@gmx.de>
2018-03-15 18:26:10 +0100

    Mirror all svn branches into shared git repository
    Add a loop over all svn branches to update the branches of the local
    git svn repository and then push to the shared git repository.
    
    Signed-off-by: Juergen Kosel <juergen.kosel@gmx.de>

--- git-svn-mirror.sh
+++ git-svn-mirror.sh
@@ -2,4 +2,13 @@
 
 git svn fetch
 git svn rebase
+git fetch --tags sharedGitRepo
 
+SVNBRANCHES=$(git branch -r --list 'origin/*')
+
+for SVNBRANCH in $SVNBRANCHES
+do
+    GITBRANCH=$(echo $SVNBRANCH | sed -e '{ s/origin\/// }')
+    git branch -f $GITBRANCH $SVNBRANCH
+    git push --tags sharedGitRepo $GITBRANCH
+done
