790542fdd3bd305a492b3931b2fafe300d19f266
Juergen Kosel <juergen.kosel@gmx.de>
2018-03-16 21:59:41 +0100

    Rename the test svn repository
    Signed-off-by: Juergen Kosel <juergen.kosel@gmx.de>

--- TestScripts/createTestSvnRepro.sh
+++ TestScripts/createTestSvnRepro.sh
@@ -3,17 +3,17 @@
 # See also https://git-scm.com/book/en/v2/Git-and-Other-Systems-Git-as-a-Client
 # and http://svnbook.red-bean.com/en/1.7/svn.reposadmin.create.html
 
-if [ -d test-svn ]
+if [ -d test-svn-repository ]
 then
-    echo "test-svn already exist -> Exiting"
+    echo "test-svn-repository already exist -> Exiting"
     exit 1
 fi
 
-mkdir test-svn
-svnadmin create test-svn
-echo "#!/bin/sh" > test-svn/hooks/pre-revprop-change
-echo "exit 0;" >> test-svn/hooks/pre-revprop-change
-chmod +x test-svn/hooks/pre-revprop-change
+mkdir test-svn-repository
+svnadmin create test-svn-repository
+echo "#!/bin/sh" > test-svn-repository/hooks/pre-revprop-change
+echo "exit 0;" >> test-svn-repository/hooks/pre-revprop-change
+chmod +x test-svn-repository/hooks/pre-revprop-change
 
 # Create standard layout
-svn mkdir -m"Create standard layout directories" file://`pwd`/test-svn/trunk file://`pwd`/test-svn/branches file://`pwd`/test-svn/tags
+svn mkdir -m"Create standard layout directories" file://`pwd`/test-svn-repository/trunk file://`pwd`/test-svn-repository/branches file://`pwd`/test-svn-repository/tags
