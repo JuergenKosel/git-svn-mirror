d1754f3192c09cc82be8118b46b5ad8a848c9ba0
Juergen Kosel <juergen.kosel@gmx.de>
2018-03-17 00:07:47 +0100

    Create the test svn repository inside of this git repository
    The test script to fill the svn repository expects the svn repository
    inside of this git repository.
    
    Signed-off-by: Juergen Kosel <juergen.kosel@gmx.de>

--- TestScripts/createTestSvnRepro.sh
+++ TestScripts/createTestSvnRepro.sh
@@ -3,17 +3,19 @@
 # See also https://git-scm.com/book/en/v2/Git-and-Other-Systems-Git-as-a-Client
 # and http://svnbook.red-bean.com/en/1.7/svn.reposadmin.create.html
 
-if [ -d test-svn-repository ]
+REPRO_ROOT=$(cd "$(dirname "$0")/.." && pwd)
+
+if [ -d ${REPRO_ROOT}/test-svn-repository ]
 then
     echo "test-svn-repository already exist -> Exiting"
     exit 1
 fi
 
-mkdir test-svn-repository
-svnadmin create test-svn-repository
-echo "#!/bin/sh" > test-svn-repository/hooks/pre-revprop-change
-echo "exit 0;" >> test-svn-repository/hooks/pre-revprop-change
-chmod +x test-svn-repository/hooks/pre-revprop-change
+mkdir ${REPRO_ROOT}/test-svn-repository
+svnadmin create ${REPRO_ROOT}/test-svn-repository
+echo "#!/bin/sh" > ${REPRO_ROOT}/test-svn-repository/hooks/pre-revprop-change
+echo "exit 0;" >> ${REPRO_ROOT}/test-svn-repository/hooks/pre-revprop-change
+chmod +x ${REPRO_ROOT}/test-svn-repository/hooks/pre-revprop-change
 
 # Create standard layout
-svn mkdir -m"Create standard layout directories" file://`pwd`/test-svn-repository/trunk file://`pwd`/test-svn-repository/branches file://`pwd`/test-svn-repository/tags
+svn mkdir -m"Create standard layout directories" file://${REPRO_ROOT}/test-svn-repository/trunk file://${REPRO_ROOT}/test-svn-repository/branches file://${REPRO_ROOT}/test-svn-repository/tags
