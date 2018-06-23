4db4223a32ad8fc312f4b8e76c09897d1cbb0c8b
Juergen Kosel <juergen.kosel@gmx.de>
2018-06-23 13:43:36 +0200

    Simplify the setting of GITBRANCH variable
    Avoid the use of echo and sed, as recommended in
    https://github.com/JuergenKosel/git-svn-mirror/commit/84f79c1fd1d7e8260a65df579747ecd81ec62fdc#comments
    
    Signed-off-by: Juergen Kosel <juergen.kosel@gmx.de>

--- git-svn-mirror.sh
+++ git-svn-mirror.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 git svn fetch
 git svn rebase
@@ -8,7 +8,7 @@ SVNBRANCHES=$(git branch -r --list 'origin/*')
 
 for SVNBRANCH in $SVNBRANCHES
 do
-    GITBRANCH=$(echo $SVNBRANCH | sed -e '{ s/origin\/// }')
+    GITBRANCH=${SVNBRANCH#origin/} # As recommended, see http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_06_02
     git branch -f $GITBRANCH $SVNBRANCH
     git push --tags sharedGitRepo $GITBRANCH
 done
